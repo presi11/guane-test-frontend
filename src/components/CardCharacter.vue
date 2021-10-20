510420c2ea52742cc9f30c1fd02fcde1fda69245
presi11 <juan.ospina19@udea.edu.co>
2021-10-20 17:42:57 -0500

    create card character with basic information

--- src/components/CardCharacter.vue
+++ src/components/CardCharacter.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
     <h1>Hola</h1>
-    <h2>{{name}}</h2>
+    <h2>{{ name }}</h2>
+    <div>
+      <b-card
+        bg-variant="dark"
+        text-variant="white"
+        :title="name"
+        :img-src="image"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2"
+        href="/"
+      >
+        <b-card-sub-title class="mb-2">Specie {{ species }} - gender {{gender}}</b-card-sub-title>
+        <b-card-text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </b-card-text>
+
+        <b-button href="/" variant="primary">Go somewhere</b-button>
+      </b-card>
+    </div>
   </div>
 </template>
 
@@ -13,11 +35,18 @@ export default {
   data: function () {
     return {
       name: null,
+      image: null,
+      status: null,
+      species: null,
+      gender: null,
     };
   },
   created() {
     axios.get("https://rickandmortyapi.com/api/character/1").then((result) => {
       this.name = result.data.name;
+      this.image = result.data.image;
+      this.species = result.data.species;
+      this.gender = result.data.gender;
       console.log(result.data);
     });
   },
